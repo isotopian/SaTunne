211f56cb937aff572ac27ce60347e90d514b0352
SaTunne <kgoldsmith@outlook.com>
2016-05-23 13:57:45 +0100

    Initial Commit

---
+++ xsproject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xsproject
+//
+//  Created by kevin_goldsmith on 23/05/2016.
+//  Copyright © 2016 kevin_goldsmith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
